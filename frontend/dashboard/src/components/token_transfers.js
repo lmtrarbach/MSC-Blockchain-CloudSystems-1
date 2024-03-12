d4c7d0a5c8155e0834129533cb2a49d6a453d193
Dafi Duck <dafiducksmc@protonmail.com>
2024-03-12 21:39:22 +0000

    add graph into dashboard, create components

--- frontend/dashboard/src/components/token_transfers.js
+++ frontend/dashboard/src/components/token_transfers.js
@@ -6,8 +6,15 @@ const TokenTransfer = () => {
     //const [data, setData] = useState({});
 
     const data = [
-        {"num_of_transactions": "74130", "total_value": "7.03596163904997E15", "received_most_value": "0x00000000000000000000000055fe002aeff02f77364de339a1292923a15844b8", "sent_most_value": "0x000000000000000000000000a9d1e08c7793af67e9d92fe308d5697fb81d3e43"}
-      ];
+      {
+        num_of_transactions: "74130",
+        total_value: "7.03596163904997E15",
+        received_most_value:
+          "0x00000000000000000000000055fe002aeff02f77364de339a1292923a15844b8",
+        sent_most_value:
+          "0x000000000000000000000000a9d1e08c7793af67e9d92fe308d5697fb81d3e43",
+      },
+    ];
     
       const config = {
         data,
